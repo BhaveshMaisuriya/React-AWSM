926fb57941b0b700ba4cc632e34fc08efaed8efc
Namrata2696 <patelnamrata2696@gmail.com>
2021-05-20 10:16:15 +0530

    jquery removed

--- src/components/Common/TableInformation/components/calendar/Weekday.js
+++ src/components/Common/TableInformation/components/calendar/Weekday.js
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from "react"
 import "react-day-picker/lib/style.css"
 import getArrayDays from "./monthArr"
-import jQuery from 'jquery'; 
 
 export default function Weekday({
   weekday,
@@ -17,11 +16,9 @@ export default function Weekday({
 
   useEffect(() => {
     const arrSelected = daynames.map(dayname => {
-      getDayOfWeek(dayname, 'add');
       const arr = getArrayDays(calendarMonth, dayname)
       return arr
     })
-
     setSelectedMulti(arrSelected.flat())
   }, [daynames.length])
 
@@ -30,26 +27,15 @@ export default function Weekday({
     const index = newDaynames.indexOf(day)
     if (index >= 0) {
       newDaynames.splice(index, 1)
-      await getDayOfWeek(day, 'remove');
     } else {
       newDaynames.push(day);
-      await getDayOfWeek(day, 'add');
     }
     setDaynames(newDaynames)
   }
 
-  const getDayOfWeek = async(selectedday, val) => {
-    var alltitle = document.getElementsByClassName("DayPicker-Weekday");
-    for (var i = 0; i < alltitle.length; i++) {
-      if(alltitle[i].getAttribute("title").toLowerCase() === selectedday.toLowerCase()){
-        val === 'add' ? await jQuery(alltitle[i]).addClass("active_day") : await jQuery(alltitle[i]).removeClass("active_day");
-      }
-    }
-  }
-
   return (
     <div
-      className={className}
+      className={`${className} ${daynames.indexOf(weekdayName) !== -1 ? 'active_day' : ''}`}
       title={weekdayName}
       style={{ cursor: "pointer" }}
       onClick={() => {

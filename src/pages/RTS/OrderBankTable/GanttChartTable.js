f530d2566c0d9eb19c5e85ce4f73e9f9aa5688b2
Namrata2696 <patelnamrata2696@gmail.com>
2021-07-08 15:37:18 +0530

    Gantt chart

--- src/pages/RTS/OrderBankTable/GanttChartTable.js
+++ src/pages/RTS/OrderBankTable/GanttChartTable.js
@@ -10,92 +10,66 @@ import {
 import "./index.scss"
 
 function GanttChartTable(props) {
-
-  const schedulerPro = new SchedulerPro({
-    appendTo : document.body,
-    flex     : '1 0 100%',
-    // Project contains all the data and is responsible for correct scheduling
-    project  : {
-        calendarsData : [
-            {
-                id        : 'general',
-                name      : '24 hour calendar',
-                intervals : [
-                    {
-                        recurrentStartDate : 'on Sat at 0:00',
-                        recurrentEndDate   : 'on Mon at 0:00',
-                        isWorking          : false
-                    }
-                ],
-                expanded : true,
-                children : [
-                    {
-                        id        : 'business',
-                        name      : 'Business hours  (8am - 5pm)',
-                        intervals : [
-                            {
-                                recurrentStartDate : 'every weekday at 12:00',
-                                recurrentEndDate   : 'every weekday at 13:00',
-                                isWorking          : false
-                            },
-                            {
-                                recurrentStartDate : 'every weekday at 17:00',
-                                recurrentEndDate   : 'every weekday at 08:00',
-                                isWorking          : false
-                            }
-                        ]
-                    },
-                    {
-                        id        : 'nightshift',
-                        name      : 'Night shift (10pm - 6am)',
-                        intervals : [
-                            {
-                                recurrentStartDate : 'every weekday at 6:00',
-                                recurrentEndDate   : 'every weekday at 22:00',
-                                isWorking          : false
-                            }
-                        ]
-                    }
-                ]
-            }
-        ],
-
-        eventsData : [
-            {
-                id       : 1,
-                name     : 'Write docs',
-                expanded : true,
-                children : [
-                    { id : 2, name : 'Read docs', startDate : '2020-11-03T14:00:00', duration : 3, durationUnit : 'h' },
-                    { id : 3, name : 'Release docs', startDate : '2020-11-03T02:00:00', duration : 4, durationUnit : 'h' }
-                ]
-            }
-        ],
-
-        resourcesData : [
-            { id : 1, name : 'Albert', calendar : 'general' },
-            { id : 2, name : 'Bill', calendar : 'nightshift'  }
-        ],
-
-        assignmentsData : [
-            { event : 2, resource : 1 },
-            { event : 3, resource : 2 }
-        ]
-    },
-    startDate  : new Date(2020, 10, 3),
-    endDate    : new Date(2020, 10, 5),
-    viewPreset : 'hourAndDay',
-    tickSize   : 50,
-    autoHeight : true,
-    columns    : [
-        { field : 'name', text : 'Name' },
-        { type : 'resourceCalendar', width : 200 }
-    ]
-});
-
   return (
     <div className="rts-table-container scroll right_border" id="scrollableDiv">
-      {schedulerPro}
+      <div className="container-orderbank" style={{ maxWidth: "100%" }}>
+        <Row className='w-100'>
+          <Col lg={5}>
+            <table className={`scrollable`}>
+              <thead>
+                <tr>
+                  {ganttChartTableColumns.map(v => {
+                    return (
+                      <th>{ganttChartTableMapping[v].label.toUpperCase()}</th>
+                    )
+                  })}
+                </tr>
+              </thead>
+              <tbody>
+                {ganttChartTableData && ganttChartTableData.length ? (
+                  ganttChartTableData.map(v => {
+                    return (
+                      <tr>
+                        {ganttChartTableColumns.map(e => {
+                          return e === "vehicle" ? (
+                            <td>
+                              <span className={`circle`}>
+                                {v[e]}{" "}
+                                {v["pto"] && (
+                                  <span className="pto_tag">{v["pto"]}</span>
+                                )}{" "}
+                              </span>
+                            </td>
+                          ) : e === "status" ? (
+                            <td>
+                              <span className={`circle`}>{v[e]}</span>
+                            </td>
+                          ) : (
+                            <td>
+                              <span className={`circle`}>{v[e]}</span>
+                            </td>
+                          )
+                        })}
+                      </tr>
+                    )
+                  })
+                ) : (
+                  <tr>
+                    <td colSpan={18} className={"rts-table-nodata"}>
+                      <div>
+                        <img src={NoDataIcon} alt="No Data" />
+                      </div>
+                    </td>
+                  </tr>
+                )}
+              </tbody>
+            </table>
+          </Col>
+          <Col lg={7}>
+            
+          </Col>
+        </Row>
+      </div>
     </div>
   )
 }
